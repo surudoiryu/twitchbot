5151d08fb4cbe9aed12be4639dfe93cddc1261f8
SurudoiRyu <nigelbloemendal@gmail.com>
2017-03-30 17:26:52 +0200

    Added socket connection with the frontend

--- src/www/www.ts
+++ src/www/www.ts
@@ -10,11 +10,13 @@ var dotenv = require("dotenv").config();
 var app = require("../app");
 var debug = require("debug")("express:server");
 var http = require("http");
+var socketIO = require("socket.io");
 var tmi = require("tmi.js");
 //var sqldb = require("fs");
 
 class TwitchBot {
 
+  private io: any;
   private server: any;
   private port: any;
 
@@ -31,6 +33,10 @@ class TwitchBot {
 
     //create http server
     this.server = http.createServer(app);
+
+    this.io = socketIO();
+    this.io.serveClient( true );
+    this.io.attach( this.server );
 
     //listen on provided ports
     this.server.listen(this.port);
@@ -70,6 +76,12 @@ class TwitchBot {
         : "Port " + this.port;
       debug("Listening on " + bind);
     });
+
+
+    this.io.on('connection', (client) => {
+      console.log('socket tunnel created with id:'+client.id);
+      client.emit('message', 'hellooo from the serverrrr sideeeeee', 'event');
+    });
   }
 
   /**
@@ -93,83 +105,3 @@ class TwitchBot {
 }
 
 let tb = new TwitchBot();
-
-//var conn = new Connect();
-//conn.connnectBot();
-
-/*
-//get port from environment and store in Express.
-var port = normalizePort(process.env.PORT || 8080);
-app.set("port", port);
-
-
-//create http server
-var server = http.createServer(app);
-
-//listen on provided ports
-server.listen(port);
-
-//add error handler
-server.on("error", onError);
-
-//start listening on port
-server.on("listening", onListening);
-*/
-
-/**
- * Normalize a port into a number, string, or false.
- */ /*
-function normalizePort(val): any {
-  var port: number = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-*/
-/**
- * Event listener for HTTP server "error" event.
- */ /*
-function onError(error : any): void {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind:string = typeof port === "string"
-    ? "Pipe " + port
-    : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-*/
-/**
- * Event listener for HTTP server "listening" event.
- */ /*
-function onListening(): void {
-  var addr:any = server.address();
-  var bind:string = typeof port === "string"
-    ? "Pipe " + port
-    : "Port " + port;
-  debug("Listening on " + bind);
-}
-*/
